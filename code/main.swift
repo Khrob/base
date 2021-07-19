c4f20be1de77b7688498f9894f56586abfc3aef9
Khrob Edmonds <khedmonds@gmail.com>
2021-07-19 15:43:17 +0800

    cc

--- code/main.swift
+++ code/main.swift
@@ -2,9 +2,9 @@
 
 func update (elapsed:Float, input:Input)
 {
-	if input.up_pressed { print ("Up!") }
-	if input.down_pressed { print ("Down!") }
-	if input.left_pressed { print ("Left!") }
+	if input.up_pressed    { print ("Up!")    }
+	if input.down_pressed  { print ("Down!")  }
+	if input.left_pressed  { print ("Left!")  }
 	if input.right_pressed { print ("Right!") }
 
 	if input.mouse_down { print (input.mouse_x, input.mouse_y) }
@@ -15,13 +15,7 @@ func render ()
 	// Nothing in here yet. 
 }
 
-// Set the base callbacks.
-update_function = update
-render_function = render
 
 // And away we go.
 open_window ()
-
-
-
 
